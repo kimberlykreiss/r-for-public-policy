a46ece186b2831f5a397dd32f33a7b8d5275ec93
Kim Kreiss <42096731+kimberlykreiss@users.noreply.github.com>
2023-02-06 11:16:30 -0500

    Add files via upload

---
+++ Workshop 1/workshop-1.R
@@ -0,0 +1,10 @@
+# This is a script 
+# You can write code here, save it, and run it when necessary 
+
+#load in libraries 
+library(tidyverse)
+
+#set working directory 
+setwd("~/Documents/git/Princeton Classes/DDSSI/R Group Workshops/Workshop 1")
+
+# Read in data 
